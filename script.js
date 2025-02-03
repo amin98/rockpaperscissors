fa4729ae2317b03c8f16588b8073e195b0431e29
Amin <499888@student.fontys.nl>
2025-02-03 08:21:37 +0100

    added more efficient code

--- script.js
+++ script.js
@@ -1,48 +1,29 @@
+const choices = ["rock", "paper", "scissors"];
+
+const winsAgainst = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
+
+function getcomputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log("It's a tie!");
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win! Rock beats scissors.");
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win! Paper beats rock.");
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win! Scissors beats paper.");
+  } else if (winsAgainst[humanChoice] === computerChoice) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
   } else {
-    console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
   }
 }
 
-function getComputerChoice() {
-  // const choices = ['rock', 'paper', 'scissors'];
-  const choice = Math.floor(Math.random() * 3);
-
-  if (choice === 0) {
-    return "rock";
-  } else if (choice === 1) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-
-}
-
-const getHumanChoice = () => {
-    for (let i = 0; i < 3; i++) {
-        if (document.getElementsByClassName("choice-button")[i]) {
-            document.getElementsByClassName("choice-button")[i].addEventListener("click", function() {
-                let humanSelection = parseInt(document.getElementsByClassName("choice-button")[i].value) - 1;
-                if (humanSelection === 0) {
-                    humanSelection = "rock";
-                } else if (humanSelection === 1) {
-                    humanSelection = "paper";
-                } else {
-                    humanSelection = "scissors";
-                }
-                const computerSelection = getComputerChoice();
-                playRound(humanSelection, computerSelection);
-            });
-        }
-    }
-}
-
-const humanSelection = getHumanChoice();
+document.querySelectorAll(".choice-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const humanChoice = choices[parseInt(button.value) - 1];
+    const computerChocie = getcomputerChoice();
+    playRound(humanChoice, computerChocie);
+  });
+});
