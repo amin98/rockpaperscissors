4ca4aba4c3de70865f1ff9c0ccbf183c1cd8d8f0
Amin <499888@student.fontys.nl>
2025-02-04 14:06:52 +0100

    removed container choicebuttons variable from js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+const choiceButton = document.querySelectorAll(".choice-button");
+
 
 const winsAgainst = {
   rock: "scissors",
@@ -11,18 +13,38 @@ function getcomputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("It's a tie!");
-  } else if (winsAgainst[humanChoice] === computerChoice) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-  } else {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-  }
+  console.log("You chose:", humanChoice);
+
+  setTimeout(() => {
+    console.log("PC chose:", computerChoice);
+  }, 1000);
+
+  setTimeout(() => {
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie!");
+    } else if (winsAgainst[humanChoice] === computerChoice) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+  }, 2000);
 }
 
-document.querySelectorAll(".choice-button").forEach((button) => {
+choiceButton.forEach((button) => {
   button.addEventListener("click", () => {
     const humanChoice = choices[parseInt(button.value) - 1];
+    button.classList.add("increaseSize");
+
+    choiceButton.forEach((btn) => {
+      if (btn !== button) {
+        btn.classList.add('disappear');
+        setTimeout(() => {
+          btn.remove();
+        }, 900);
+      }
+    });
+
+
     const computerChocie = getcomputerChoice();
     playRound(humanChoice, computerChocie);
   });
